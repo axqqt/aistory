c74a1f3bb4c0741717f6c1d13229737bf9936cd4
Veloxal <127298642+axqqt@users.noreply.github.com>
2024-09-03 14:46:13 +0530

    qwtqwt

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,12 @@
 // app/layout.js
 
-import Link from 'next/link';
-import './globals.css';
+import Link from "next/link";
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <nav className="bg-gray-800 p-4 text-white">
-          {/* <Link href="/">Home</Link> | <Link href="/story">Create Story</Link> */}
-        </nav>
         <div>{children}</div>
       </body>
     </html>
